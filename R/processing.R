8ef5f700da8fc751282e86c94013107373ea8314
Frederick Solt <fsolt@mac.com>
2017-01-18 13:06:09 -0600

    fix file paths (use data dir, not results); drop extraneous imports

--- R/processing.R
+++ R/processing.R
@@ -1,16 +1,11 @@
 library(tidyverse)
-library(foreign)
-library(haven)
-library(reshape2)
-library(beepr)
-library(rstan)
 library(stringr)
 library(DCPO)
 
 gm <- dcpo_setup(vars = "data-raw/surveys_gm.csv",
-                 file = "results/all_data_gm.csv")
+                 file = "data/all_data_gm.csv")
 
-gm <- read_csv("results/all_data_gm.csv")
+gm <- read_csv("data/all_data_gm.csv")
 
 x <- gm %>% with_min_yrs(3)
 out1 <- dcpo(x, iter = 4000)
