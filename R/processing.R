5bb28e4fe2a46bb1e0f5ec3182d57bb97e62b428
Frederick Solt <fsolt@mac.com>
2017-08-04 10:28:19 -0500

    add runtime clock

--- R/processing.R
+++ R/processing.R
@@ -7,10 +7,12 @@ gm <- dcpo_setup(vars = "data-raw/surveys_gm.csv",
 
 gm <- read_csv("data/all_data_gm.csv")
 
+start <- proc.time()
 x <- gm %>% with_min_yrs(3)
 out1 <- dcpo(x, iter = 4000)
 save(out1, file = str_c("data/gm_", str_replace(Sys.time(), " ", "_"), ".rda"))
-
+runtime <- proc.time() - start
+runtime
 
 ab <- dcpo_setup(vars = "data-raw/surveys_abortion.csv",
                  file = "data/all_data_abortion.csv")
