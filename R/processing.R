e7752ebc96f06d2a6b1babb5b6f6f7000cd6b401
Frederick Solt <fsolt@mac.com>
2018-07-08 14:11:45 -0500

    update

--- R/processing.R
+++ R/processing.R
@@ -13,6 +13,48 @@ save(x, out1, file = str_c("data/gm_", str_replace(Sys.time(), " ", "_"), ".rda"
 runtime <- proc.time() - start
 runtime
 
+x1 <- rstan::summary(out1)
+x1_sum <- as.data.frame(x1$summary)     # as.data.frame() preserves rownames
+x1_sum$parameter <- rownames(x1_sum)
+x1_sum$parameter_type <- gsub("([^[]*).*", "\\1", x1_sum$parameter)
+
+x2 <- x %>% 
+    mutate(prob = y_r/n,
+           prob_se = sqrt(prob*(1-prob)/n)) %>% 
+    bind_cols(x1_sum %>% 
+                  filter(parameter_type=="pred_prob") %>% 
+                  transmute(pred_prob = mean, 
+                            pred_prob_se = sd)) %>% 
+    group_by(variable_cp) %>% 
+    mutate(vcp_mean = mean(prob)) %>% 
+    ungroup() %>% 
+    mutate(diff = prob - pred_prob,
+           diff_se = sqrt(prob_se^2+pred_prob_se^2),
+           diff_t = abs(diff/diff_se),
+           well_predicted = diff_t < 1.96,
+           diff_vcp_mean = prob - vcp_mean)
+
+r2_by_item <- x2 %>% 
+    group_by(variable_cp) %>%
+    summarise(rmsr = sqrt(mean(diff^2)),
+              rmsr_item_means_only = mean(abs(diff_vcp_mean)),
+              r2_by_item = 1 - (sum(diff^2)/sum((prob - mean(prob))^2)),
+              prob = mean(prob), 
+              pred_prob = mean(pred_prob),
+              diff = mean(abs(diff)))
+
+overall_fit <- x2 %>% 
+    summarize(rmsr = sqrt(mean(diff^2)),
+              rmsr_item_means_only = mean(abs(diff_vcp_mean)),
+              by_item_r2 = 1 - (sum(diff^2)/sum(diff_vcp_mean^2)))
+
+p <- x$y_r/x$n
+
+beepr::beep()
+
+shinystan::launch_shinystan(out1)
+
+
 ab <- dcpo_setup(vars = "data-raw/surveys_abortion.csv",
                  file = "data/all_data_abortion.csv")
 
