dacbb66b9770521ddc22d9d61b486842dd0b99bd
Frederick Solt <frederick-solt@uiowa.edu>
2019-06-15 15:33:50 -0500

    update

--- R/ts_plot.R
+++ R/ts_plot.R
@@ -6,10 +6,26 @@ cols <- 5
 npages <- 3
 #a_res <- df
 
+t_res <- rstan::summary(out1, pars="bar_theta", probs=c(.1, .9)) %>%
+  first() %>%
+  as.data.frame() %>%
+  rownames_to_column("parameter") %>%
+  as_tibble() %>%
+  mutate(year = str_extract(parameter, "\\d+(?=,)") %>% 
+           as.numeric() %>% 
+           factor(labels = attr(dcpo_data$N, "dimnames")[[1]]) %>% 
+           as.character() %>% 
+           as.numeric(),
+         country = str_extract(parameter, "(?<=,)\\d+") %>% 
+           as.numeric() %>% 
+           factor(labels = attr(dcpo_data$N, "dimnames")[[2]]) %>% 
+           as.character()) %>% 
+  arrange(country, year)
+
 pages <- c("1:35", "36:70", "71:105")
-t_res1 <- t_res1 %>%
+t_res1 <- t_res %>%
   group_by(country) %>%
-  mutate(last_est = last(estimate)) %>%
+  mutate(last_est = last(mean)) %>%
   ungroup()
 for (i in 1:npages) {
   cpage <- unique(t_res1$country)[((i-1)*rows*cols)+1:i*rows*cols]
@@ -17,12 +33,12 @@ for (i in 1:npages) {
   cp <- t_res1[t_res1$country %in% cpage, ]
   cp$country <- factor(cp$country, levels = cpage)
 
-  plotx <- ggplot(data=cp, aes(x=year, y=estimate)) +
+  plotx <- ggplot(data=cp, aes(x=year, y=mean)) +
     geom_line() + theme_bw() +
     theme(legend.position="none") +
-    coord_cartesian(xlim=c(1980,2016)) +
+    coord_cartesian(xlim=c(1973,2017)) +
     labs(x = NULL, y = "Tolerance") +
-    geom_ribbon(aes(ymin = lb, ymax = ub, linetype=NA), alpha = .25) +
+    geom_ribbon(aes(ymin = `10%`, ymax = `90%`, linetype=NA), alpha = .25) +
     facet_wrap(~country, ncol = 5) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1),
           strip.background = element_rect(fill = "white", colour = "white"))
